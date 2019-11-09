9ee5edb55d36894decfd7946c9754ef73ef42e24
dalexach <dalexach@gmail.com>
2019-11-09 00:18:47 +0000

    Fixed Serialization and Deserialization with JSON

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,39 +7,33 @@ import json
 import os.path
 
 class FileStorage():
-	"""
-	"""
-	__file_path = "file.json"
-	__objects = {}
+    """
+    """
+    __file_path = "file.json"
+    __objects = {}
 
-	def all(self):
-		"""
-		"""
-		return self.__objects
+    def all(self):
+        """
+        """
+        return self.__objects
 
-	def new(self, obj):
-		"""
-		"""
-		if obj:
-			obj_class = type(obj).__name__
-			obj_id = obj.id
-			self.__objects.update({str(obj_class + '.' + obj.id): obj.to_dict})
+    def new(self, obj):
+        """
+        """
+        if obj:
+            obj_dict = obj.to_dict()
+            obj_key = obj_dict['__class__'] + '.' + obj_dict['id']
+            self.__objects[obj_key] = obj_dict
 
-	def save(self):
-		"""
-		"""
-		with open(self.__file_path, 'w', encoding="utf-8") as json_file:
-			new_dict = {}
-			for key, value in self.__objects.items():
-				#new_dict.update(key = dict(value))
-				print(type(value))
-			json_file.write(json.dumps(new_dict))
+    def save(self):
+        """
+        """
+        with open(self.__file_path, 'w') as json_file:
+            json_file.write(json.dumps(self.__objects))
 
-	def reload(self):
-		"""
-		"""
-		if os.path.exists(self.__file_path):
-			with open(self.__file_path, 'r') as json_file:
-				string = json_file.read()
-				if len(string) != 0:
-					self.__objects = json.loads(string)
+    def reload(self):
+        """
+        """
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as json_file:
+                self.__objects = json.loads(json_file.read())
