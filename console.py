375ff66a87d9326580e9908567db25739946fbec
espinosakev24 <916@holbertonschool.com>
2019-11-12 19:25:09 -0500

    uploading console with all commands implemented

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 Console module
 """
 import cmd
+from datetime import datetime
 from models import all_classes
 from models import storage
 
@@ -39,6 +40,7 @@ class HBNBCommand(cmd.Cmd):
             check = 0
             try:
                 class_name = inputs.split(" ")[0]
+                cl = all_classes[class_name]
                 try:
                     object_id = inputs.split(" ")[1]
                 except Exception:
@@ -63,30 +65,104 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, inputs):
         """
         """
-        try:
-            class_name = inputs.split(" ")[0]
+        if not(inputs):
+            print("** class name missing **")
+        else:
+            check = 0
             try:
-                if all_classes[inputs]:
-                    print("Exitst")
+                class_name = inputs.split(" ")[0]
+                cl = all_classes[class_name]
+                try:
+                    object_id = inputs.split(" ")[1]
+                except Exception:
+                    print("** instance id missing **")
+                    return
+                if all_classes[class_name]:
+                    all_objs = storage.all()
+                    for key in all_objs.keys():
+                        obj = all_objs[key]
+                        if obj.id == object_id:
+                            check = 1
+                            del all_objs[class_name + '.' + obj.id]
+                            storage.save()
+                            break
+                    if check == 0:
+                        print("** no instance found **")
             except Exception:
                 print("** class doesn't exist **")
-                return
-            try:
-                object_id = inputs.split(" ")[1]
-            except Exception:
-                print("** instance id missing **")
-                return
-            all_objs = storage.all()
-            for key in all_objs[key]
-                obj = all_objs[key]
-                if obj.id == object_id:
-        except Exception:
-            print("** class name missing **")
 
     def destroy_help(self):
         """
         """
-        pass    
+        pass
+
+    def do_all(self, inputs):
+        """
+        """
+        all_objs = storage.all()
+        if not(inputs):
+            for key, value in all_objs.items():
+                print([str(value)])
+            return
+        try:
+            cl = all_classes[inputs]
+        except Exception:
+            print("** class doesn't exist **")
+            return
+        for key in all_objs.keys():
+            if key.split('.')[0] == inputs:
+                print([str(all_objs[key])])
+
+    def all_help(self):
+        """
+        """
+        pass
+
+    def do_update(self, inputs):
+        """
+        """
+        if not(inputs):
+            print("** class name missing **")
+        else:
+            check = 0
+            try:
+                class_name = inputs.split(" ")[0]                    
+                try:
+                    object_id = inputs.split(" ")[1]
+                except Exception:
+                    print("** instance id missing **")
+                    return
+                if all_classes[class_name]:
+                    all_objs = storage.all()
+                    for key in all_objs.keys():
+                        obj = all_objs[key]
+                        if obj.id == object_id:
+                            check = 1
+                    if check == 0:
+                        print("** no instance found **")
+            except Exception:
+                print("** class doesn't exist **")
+                return
+            obj_key = class_name + '.' + object_id
+            obj = all_objs[obj_key]
+            try:
+                atr = inputs.split(" ")[2]
+            except Exception:
+                print("** attribute name missing **")
+                return
+            try:
+                value_name = inputs.split(" ")[3]
+            except Exception:
+                print("** value missing **")
+                return
+            if atr != 'created_at' or atr != 'update_at' or atr != 'id':
+                obj.__dict__.update({atr: value_name})
+                obj.save()
+    def help_update(self):
+        """
+        """
+        pass
+
     def do_quit(self, inputs):
         """ Exit the console when the input is exit """
         return True
